513a1a800433b42fb9c0034c602eecca48557f97
Crowdbotics <team@crowdbotics.com>
2022-01-07 15:36:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "passwordgenerator_32731",
+  url: "https://passwordgenerator_32731.botics.co",
+  api: "https://passwordgenerator_32731.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
